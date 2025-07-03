221c2ae09966d02de0cb5778e3b3160a76dd69b6
Alexandr <kamo19@yandex.ru>
2025-07-04 00:41:30 +0300

    Добавлен метод PUT и обновлены основные файлы

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,78 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/alexandr90-tech/task-tracker/internal/task"
 )
 
 func main() {
+
+	store := task.NewStore()
+
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Server is alive")
 	})
 
+	http.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			tasks := store.ListTasks()
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(tasks)
+		case "POST":
+			var input struct {
+				Title string `json:"title"`
+			}
+
+			err := json.NewDecoder(r.Body).Decode(&input)
+			if err != nil || input.Title == "" {
+				http.Error(w, "Invalid input", http.StatusBadRequest)
+				return
+			}
+			task := store.AddTask(input.Title)
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusCreated)
+			json.NewEncoder(w).Encode(task)
+		case "PUT":
+			idStr := r.URL.Query().Get("id")
+			if idStr == "" {
+				http.Error(w, "Missing id parameter", http.StatusBadRequest)
+				return
+			}
+			id, err := strconv.Atoi(idStr)
+			if err != nil {
+				http.Error(w, "Invalid id parameter", http.StatusBadRequest)
+				return
+			}
+
+			var input struct {
+				Done bool `json:"done"`
+			}
+
+			if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+				http.Error(w, "Invalid input", http.StatusBadRequest)
+				return
+			}
+
+			task, ok := store.UpdateTaskDone(id, input.Done)
+			if !ok {
+				http.Error(w, "Task not found", http.StatusNotFound)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(task)
+
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+
 	fmt.Println("Starting server on :8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- internal/task/task.go
+++ internal/task/task.go
@@ -3,44 +3,43 @@ package task
 import "sync"
 
 type Task struct { // структура одной задачи
-	ID int 'json:"id"'
-	Tittle string 'json:"title"'
-	Done bool 'json:"done"'
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
 }
 
 type Store struct { // хранилище задач в оперативной памяти (без БД)
-	sync.Mutex
-	tasks []Task
-	nextID int
+	sync.Mutex // чтобы защититься от одновременного доступа
+	tasks      []Task
+	nextID     int
 }
 
 func NewStore() *Store {
 	return &Store{
-	tasks: []Task{},
-	nextID: 1,
+		tasks:  []Task{},
+		nextID: 1,
 	}
 
 }
 
-func (s *Store) ListTasks() []Task {
-s.Lock()
-defer s.Unlock()
-return append([]Task(nil), s.tasks...)
+func (s *Store) ListTasks() []Task { // возвращает список всех задач
+	s.Lock()
+	defer s.Unlock()
+	return append([]Task(nil), s.tasks...)
 }
 
-func (s *Store) AddTask(title string) Task{
-s.Lock()
-defer s.Unlock()
+func (s *Store) AddTask(title string) Task { // добавление задачи
+	s.Lock()
+	defer s.Unlock()
 
+	task := Task{
+		ID:    s.nextID,
+		Title: title,
+		Done:  false,
+	}
 
-task := Task{
-ID: s.NextID,
-Title: title,
-Done: false,
-}
-
-s.tasks = append(s.tasks, task)
-s.nextID++
-return task
+	s.tasks = append(s.tasks, task)
+	s.nextID++
+	return task
 
 }
